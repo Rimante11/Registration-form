4d4b7521868a33772ff420a8c35cece0c6973b56
rimante _ <storulike@gmail.com>
2022-02-03 17:33:42 +0100

    fixar

--- main.js
+++ main.js
@@ -1,73 +1,27 @@
-console.log("My form exempel med varning meddelande för värje input");
-const myform = document.getElementById("form");
-const inputs = myform.querySelectorAll("input");
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const btnSubmit = document.getElementById("submit");
-const allErrors = document.querySelectorAll(".errors");
+const registerform = document.getElementById('registerform');
+const allErrors = document.querySelectorAll('.error');
 
-function hideErrors(){
-  allErrors.forEach((eatchErrorElement) => {
-    eatchErrorElement.style.display = "none";
+function hideErrors() { 
+  allErrors.forEach((errorElem) => { 
+    errorElem.style.display = 'none';
   });
 }
-//hideErrors(); //invackna funktion om det funkar
 
+registerform.addEventListener('submit', (e) => { 
+  e.preventDefault();
 
-myform.addEventListener("submit", e => {
-  //e.preventDefault(); //om man vill stoppa browserns vanliga betyende
   hideErrors();
 
-  const inputs = form.querySelectorAll("input");
-  inputs.forEach((input) => {
-
-    if(!input.checkValidity()){
-      const errorElm = document.getElementById(input.id + "errors");
-      errorElm.style.display = "block";
+  const inputs = registerform.querySelectorAll('input');
+  inputs.forEach((input) => { 
+    if (!input.checkValidity()) { 
+      const errorElem = document.getElementById(input.id + 'error');
+      errorElem.style.display = 'block';
     }
-  })
-  /*
-  const termBox = document.getElementsByTagName("accept");
-  console.log(username.value);//ser inputs i conslen
-  console.log(password.value);
-  console.log(termBox.checked); //för checkbox men var undefaned istället av true false
-  */
+  });
+
 });
+
 hideErrors();
-/*
-btnSubmit.addEventListener("click", event => {
-  console.log("Button clicked.")
-});
-*/
-//Denna metod är bra om man vill tex on click på field visa up requirements
-//exempel min 8 bokstaver osv
-/*
-username.addEventListener("focus", () => {
-  console.log("username field has focus by click on username field");
-});
-*/
 
-////AFTER 3s FOCUS ON USERNAME FIELD
-/* 
-setTimeout(() => {
-  username.focus();
-}, 3000); //sets username field in focus efter 3s page was refreshed
-*/
-/////////////////////////////////////////////////////////////////////////////
-/*
-//sätta focus to input fälltet
-function setFocusToInputField(){
-  var inputs = document.getElementsByTagName("input");
-  inputs.focus();
 
-}
-setFocusToInputField();
-/*
-form.addEventListener("submit", (e)=> {
-  e.preventDefault(); //preventing from submiting
-
-  validateInputs();
-});
-*/
